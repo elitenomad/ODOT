5da351919de02bf50c8c9c3cc23c56ae26c12860
elitenomad <stalin.pranava@gmail.com>
2014-04-13 08:34:19 +1000

    Tests for the Todoitems index added

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   def index
-
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_items = @todo_list.todo_items
   end
 end
--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,14 +2,33 @@ require 'spec_helper'
 
 describe TodoItem do
   let!(:todo_list) { TodoList.create(title:'My first todo', description:'jujujujubi') }
-  it 'should return empty when no items are present' do
 
-
+  def visit_todo_list(list)
     visit '/todo_lists'
     within "#todo_list_#{todo_list.id}" do
       click_link 'List items'
     end
+  end
 
-    expect(page).to have_content("TodoItems#index")
+  it "should display title on the pages" do
+    visit_todo_list(todo_list)
+    within "h1" do
+      expect(page).to have_content(todo_list.title)
+    end
+  end
+
+  it 'should return empty when no items are present' do
+    visit_todo_list(todo_list)
+    expect(page).to have_content("No Items found")
+  end
+
+  it 'should return items in a table when not empty' do
+    item1 = todo_list.todo_items.create(content:'have to propose')
+    item2 = todo_list.todo_items.create(content:'have to do homework')
+    item3 = todo_list.todo_items.create(content:'have to play cricket')
+    visit_todo_list(todo_list)
+
+    expect(page.all("table.table tr").size).to eq(3)
+
   end
 end
