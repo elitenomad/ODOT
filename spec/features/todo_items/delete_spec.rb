8bf2c5fc101d602d531c3905b1c24af5dd0ccc22
elitenomad <stalin.pranava@gmail.com>
2014-04-13 11:27:40 +1000

    Tests for deleting todo item

--- spec/features/todo_items/delete_spec.rb
+++ spec/features/todo_items/delete_spec.rb
@@ -1,7 +1,26 @@
 require 'spec_helper'
 
 
-describe "Delete todo_list Items" do
+describe 'Deleting Todo List Items' do
+  let!(:todo_list) { TodoList.create(title:'My first todo', description:'jujujujubi') }
+  let!(:todo_item) { todo_list.todo_items.create(content: 'Marriage anniversary') }
 
+  def visit_todo_list(list)
+    visit '/todo_lists'
+    within "#todo_list_#{todo_list.id}" do
+      click_link 'List items'
+    end
+  end
 
+  it "should delete todo item successfully" do
+    visit_todo_list(todo_list)
+
+    within("#todo_item_#{todo_item.id}") do
+      click_link 'Delete Item'
+    end
+
+    expect(page).to have_content(todo_list.title)
+
+    expect(todo_list.todo_items.size).to eq(0)
+  end
 end
